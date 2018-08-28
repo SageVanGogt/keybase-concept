ce1b2794c599369ae3c8bda8b1a98849dc21fbc2
Sage Vogt <thurmanvogt@gmail.com>
2018-08-28 17:26:38 -0600

    Add redux setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+render(
+  <Provider store={store} >
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
