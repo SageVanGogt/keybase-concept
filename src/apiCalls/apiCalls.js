3d82b0ce4fd3e0a7847d8724d177ff4713535bce
Sage Vogt <thurmanvogt@gmail.com>
2018-08-28 16:30:50 -0600

    Add fetchGeoCode apiCall

---
+++ src/apiCalls/apiCalls.js
@@ -0,0 +1,13 @@
+const fetchGeocode = async (address) => {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEO_KEY}`;  
+  try {
+    const response = await fetch(url);
+    const locationData = await response.json();
+    if (!locationData) {
+      return new Error('error')
+    }
+    return locationData;
+  } catch (error) {
+    throw error;
+  }
+}; 
