0ef783097e6cfa4edb0acb8bf4b8133137a0bab6
Sage Vogt <thurmanvogt@gmail.com>
2018-08-28 16:32:30 -0600

    Add reverseGeoCode testing

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -24,3 +24,37 @@ describe('fetchGeocode', () => {
     expect(actual).toEqual(expected);
   })
 });
+
+describe('reverseGeoCode', () => {
+  let url;
+  let mockLat;
+  let mockLng;
+
+  beforeEach(() => {
+    mockLat = 23;
+    mockLng = 23;
+    url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${mockLat},${mockLng}&key=${geoKey}`;
+    window.fetch = jest.fn().mockImplementation(() => 
+      Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve(MOCK.mockReverseGeoInfo)
+      })
+    );
+  });
+
+  it('should be called with the correct params', async () => {
+    let expected = url;
+
+    await API.reverseGeoCode(mockLat, mockLng);
+
+    expect(window.fetch).toHaveBeenCalledWith(expected);
+  });
+
+  it('should return the expected object', async () => {
+    let expected = MOCK.mockReverseGeoInfo;
+
+    let actual = await API.reverseGeoCode(mockLat, mockLng);
+
+    expect(actual).toEqual(expected);
+  });
+});
